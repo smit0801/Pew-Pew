97621cc61a201c414f26b7e1c4318abe4a2db5f7
smit0801 <smitmhatre0801@gmail.com>
2026-01-31 03:31:17 -0500

    Base -Done

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+from logger import log_event
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
+
+
+    clock = pygame.time.Clock()
+    dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
     while True:
         # Log the game state
         log_state()
@@ -19,9 +43,24 @@ def main():
 
         # Draw the screen
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+        for obj in drawable:
+            obj.draw(screen)
 
         # Update the display (must be last)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        # print(dt)
 if __name__ == "__main__":
     main()
